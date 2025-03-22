ad760d668c1ad0dddb17f7a286581073313cef5e
Andrushkon <andrushcon@gmail.com>
2025-03-22 16:25:23 +0000

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,46 @@
 const listadepalabras=[
-    ["Añejo",["hola"],"https://cdn.pixabay.com/photo/2021/12/19/10/42/old-6880626_1280.jpg"],
-    ["Aseveró",["chino"],"https://cdn.pixabay.com/photo/2013/07/13/10/06/affirmative-156538_1280.png"]
-    ["bocanada",["chino"]]
+    ["Añejo",["adj. Dicho de una cosa: Que tiene uno o más años. Tocino, vino añejo"," adj. coloq. Que tiene mucho tiempo. Vicio añejo. Noticia añeja"],"https://cdn.pixabay.com/photo/2021/12/19/10/42/old-6880626_1280.jpg",25],
+    ["Aseveró",["tr. Afirmar o asegurar lo que se dice"],"https://cdn.pixabay.com/photo/2013/07/13/10/06/affirmative-156538_1280.png",64],
+    ["Bocanada",["f. Cantidad de aire o de otro fluido que de una vez se toma en la boca o se arroja de ella","f. Cantidad de líquido que refluye a la boca con una arcada","f. Ráfaga de aire, de olor o de calor, que llega y cesa de repente. Una bocanada de perfume"],"https://cdn.pixabay.com/photo/2024/03/06/05/02/balloon-8615695_1280.png",10],
+    ["Bóveda",["f. Arq. Obra de fábrica arqueada, que sirve para cubrir el espacio entre dos apoyos y forma el techo o la cubierta de una construcción","f. Habitación labrada sin madera alguna, cuya cubierta o parte superior es de bóveda","f. sepultura (‖ lugar en que está enterrado un cadáver)","f. Arg. Panteón familiar"],"https://cdn.pixabay.com/photo/2020/04/07/15/07/vault-5013752_1280.png",115],
+    ["Chupadero",["adj. Dicho de una cosa: Que chupa","m. chupador (‖ pieza para que chupen los niños)"],"https://cdn.pixabay.com/photo/2016/06/02/22/14/ice-1432278_1280.png",24],
+    ["Coordenadas",["adj. Geom. Dicho de una línea, de un plano o de un eje: Que sirve para determinar la posición de un punto en el plano o en el espacio","f. Cada uno de los números que determinan la posición de un punto en el plano o en el espacio"],"https://cdn.pixabay.com/photo/2013/07/12/13/43/earth-147171_1280.png",91],
+    ["Dilataron",[" tr. Extender, alargar y hacer mayor algo, o que ocupe más lugar o tiempo","tr. aplazar (‖ retrasar)","tr. propagar (‖ extender el conocimiento o la afición). Dilatar la fama, el nombre","prnl. Extenderse mucho en un discurso o escrito"],"https://cdn.pixabay.com/photo/2014/04/03/11/46/eyes-312093_1280.png",30],
+    ["Discernir",["tr. Distinguir algo de otra cosa, señalando la diferencia que hay entre ellas. Comúnmente se refiere a operaciones del ánimo","tr. Conceder u otorgar un cargo, distinción u honor"],"https://cdn.pixabay.com/photo/2013/07/12/14/11/unequal-147925_1280.png",98],
+    ["Esparto",["m. Planta de la familia de las gramíneas, con las cañas de unos 70 cm de altura, hojas radicales de unos 60 cm de longitud, tan arrolladas sobre sí y a lo largo que aparecen como filiformes, duras y tenacísimas, que tiene flores en panoja espigada de 30 cm de largo, y semillas muy menudas","m. Hojas del esparto, empleadas en la industria para hacer sogas, esteras, tripe, pasta para fabricar papel, etc"],"https://cdn.pixabay.com/photo/2021/11/25/18/46/leaf-6824367_1280.png",24],
+    ["Eufórica",["adj. Perteneciente o relativo a la euforia"],"https://cdn.pixabay.com/photo/2018/01/05/17/06/silhouette-3063375_1280.png",103],
+    ["Fisura",["f. Grieta que se produce en un objeto","f. Geol. Hendidura que se encuentra en una masa mineral","f. Med. Hendidura de un hueso, que no llega a romperlo","f. Med. Grieta en el ano"],"https://cdn.pixabay.com/photo/2012/04/18/15/40/pavement-37368_1280.png",70],
+    ["Folio",["m. Hoja de un libro o de un cuaderno","m. Titulillo o encabezamiento de las páginas de un libro","m. Hoja de papel que resulta de doblar una vez el pliego de marca ordinaria","m. Hierba dioica de la familia de las euforbiáceas, que tiene las hojas aovadas y cubiertas de una especie de tomento blanco, con el tallo algo leñoso, las flores conglobadas y las semillas casi redondas"],"https://cdn.pixabay.com/photo/2013/07/12/13/28/flash-card-147112_1280.png",35],
+    ["Galán",["m. Hombre de buen semblante, bien proporcionado y airoso en el manejo de su persona","m. Hombre que galantea a una mujer","m. Actor de teatro o de cine, de buena presencia y generalmente joven, que representa papeles principales de hombre enamorado o galanteador"],"https://cdn.pixabay.com/photo/2018/05/14/11/39/darkness-3399600_1280.png",96],
+    ["Gruta",["f. Caverna natural o artificial","f. Estancia subterránea artificial que imita más o menos los peñascos naturales"],"https://cdn.pixabay.com/photo/2021/03/22/23/30/cave-6115975_1280.png",109],
+    ["Hostia",["f. Lámina redonda y delgada de pan ácimo que, conforme a la religión cristiana, se consagra en la misa y con la que se comulga","f. malson. Esp. Golpe o bofetada"],"https://cdn.pixabay.com/photo/2014/04/03/10/24/volleyball-310328_1280.png",52],
+    ["Huyendo",["intr. Alejarse deprisa, por miedo o por otro motivo, de personas, animales o cosas, para evitar un daño, disgusto o molestia","intr. Dicho de una cosa: Alejarse velozmente. La nave huye del puerto","intr. Dicho de unidades de tiempo: Transcurrir o pasar velozmente. Huyen los siglos, la vida","intr. Apartarse de algo malo o perjudicial. Huir de los atascos de tráfico"],"https://cdn.pixabay.com/photo/2018/03/04/22/43/silhouette-3199472_1280.png",130],
+    ["Incoloro",["adj. Que carece de color"],"https://cdn.pixabay.com/photo/2013/07/12/19/20/rainbow-colors-154569_1280.png",114],
+    ["Irradiaba",["tr. Dicho de un cuerpo: Despedir rayos de luz, calor u otra energía"],"https://cdn.pixabay.com/photo/2013/07/13/10/23/sun-157126_1280.png",56],
+    ["Joder",["intr. malson. Practicar el coito","prnl. malson. Aguantarse o fastidiarse","prnl. malson. Estropearse o dañarse","tr. malson. Poseer sexualmente a alguien","tr. malson. Molestar o fastidiar a alguien","tr. malson. Destrozar, arruinar o echar a perder algo","interj. malson. U. para expresar enfado, irritación, asombro, etc"],"https://cdn.pixabay.com/photo/2014/03/24/13/47/swearing-294391_1280.png",61],
+    ["Jóvenes",["dj. Dicho de una persona: Que está en la juventud","adj. De poca edad, frecuentemente considerado en relación con otros. El más joven de todos era yo","adj. Propio de una persona joven. Una manera joven de vivir","adj. Dicho de un animal: Que aún no ha llegado a la madurez sexual, o, si se desarrolla con metamorfosis, que ha alcanzado la última fase de esta y el aspecto de los adultos","adj. Que conserva características propias de una persona joven","adj. U. precedido del artículo el y pospuesto al nombre o al apellido de una persona para indicar que esta nació después que otra llamada igual. Lucas Cranach, el joven"],"https://cdn.pixabay.com/photo/2013/07/12/17/17/playschool-151938_1280.png",133],
+    ["Ladera",["adj. Perteneciente o relativo al lado","m. Arg. Persona que secunda a otra, particularmente a un caudillo político","f. Declive de un monte o de una altura"],"https://cdn.pixabay.com/photo/2022/11/30/05/38/people-7625904_1280.png",127],
+    ["Lúgubre",["adj. fúnebre (‖ muy triste)","adj. Sombrío, profundamente triste"],"https://cdn.pixabay.com/photo/2017/01/30/23/58/ghost-2022413_1280.png",126],
+    ["Membrete",["m. Nombre o título de una persona, oficina o corporación, estampado en la parte superior del papel de escribir","m. Nombre o título de una persona o corporación puesto a la cabeza de la primera plana o al final del escrito que a esta misma persona o corporación se dirige","m. p. us. Memoria o anotación que se hace de algo, poniendo solo lo sustancial y preciso, para copiarlo y extenderlo después con todas sus formalidades y requisitos","m. p. us. Aviso por escrito o nota en que se hace un convite o se recomienda o recuerda una pretensión"],"https://cdn.pixabay.com/photo/2016/06/13/09/57/meeting-1453895_960_720.png",79],
+    ["Mortecino",["adj. Bajo, apagado y sin vigor","adj. Que está casi muriendo o apagándose","adj. p. us. Dicho de un animal: Que ha muerto naturalmente","adj. Dicho de una carne: De un animal mortecino"],"https://cdn.pixabay.com/photo/2015/02/11/16/42/crow-632660_1280.png",17],
+    ["Ni",["conj. copulat. U. para coordinar de manera aditiva vocablos o frases que denotan negación, precedida o seguida de otra u otras igualmente negativas. No como ni duermo","conj. copulat. En cláusula que empieza con verbo precedido del adverbio no y en que se niegan dos o más términos, puede omitirse o expresarse delante del primero. No descansa de día ni de noche","conj. copulat. Ni siquiera. U. encabezando una oración sin relacionarla con otra o con relación distinta de la copulativa negativa 'y no'. Forma frases que expresan el extremo a que puede llegarse en algo. Eso no te lo crees ni tú","conj. copulat. U. con valor semejante en exclamaciones enérgicas. ¡Ni soñarlo! ¡Ni mucho menos","conj. copulat. No. U. sin otra negación anterior o posterior. Perdió el caudal y la honra; ni podía esperarse otra cosa de su conducta","conj. copulat. desus. Era u. también como la conjunción disyuntiva o. ¿Te hablé yo ni te vi?"],"https://cdn.pixabay.com/photo/2013/07/12/13/50/prohibited-147408_1280.png",66],
+    ["Nordeste",["m. Punto del horizonte entre el norte y el este, a igual distancia de ambos. (Símb. NE)","m. Lugar situado al nordeste de otro con cuya posición se compara. Las Bahamas se encuentran al nordeste de Cuba","m. U. en aposición para indicar que lo designado por el sustantivo al que se pospone está orientado al nordeste o procede del nordeste. Rumbo, viento nordeste","m. Viento procedente del nordeste"],"https://cdn.pixabay.com/photo/2013/07/12/17/10/compass-151722_1280.png",112],
+    ["Ojipláticos",["adj. coloq. Esp. Con los ojos como platos, muy asombrado o sorprendido. Me quedé ojiplática cuando me dijo que se divorciaba"],"https://cdn.pixabay.com/photo/2016/12/18/11/04/eye-1915455_1280.png",28],
+    ["Optimismo",["m. Propensión a ver y juzgar las cosas en su aspecto más favorable","m. Fil. Doctrina que atribuye al universo la mayor perfección posible"],"https://cdn.pixabay.com/photo/2022/03/13/10/39/opportunity-7065790_1280.png",115],
+    ["Performance",["f. rendimiento (‖ proporción entre el resultado obtenido y los medios utilizados)","f. Actividad artística que tiene como principio básico la improvisación y el contacto directo con el espectador"],"https://cdn.pixabay.com/photo/2016/06/01/10/28/musicians-1428749_1280.png",75],
+    ["Primitivo",["adj. Primero en su línea, o que no tiene ni toma origen de otra cosa","adj. Perteneciente o relativo a los orígenes o primeros tiempos de algo","adj. Dicho de un individuo o de un pueblo: De civilización poco desarrollada","adj. Perteneciente o relativo a un pueblo primitivo","adj. Rudimentario o elemental","adj. Esc. y Pint. Dicho de un artista o de una obra artística: De una época anterior a la que se considera clásica dentro de una civilización o un ciclo, y en especial anterior al Renacimiento o a su influjo","adj. Gram. Dicho de una palabra: Que no se deriva de otra de la misma lengua","f. lotería primitiva"],"https://cdn.pixabay.com/photo/2024/04/04/12/14/ai-generated-8675008_1280.png",52],
+    ["Regazo",["m. Parte del cuerpo comprendida entre la cintura y las rodillas de una persona sentada. Cruzaba las manos sobre su regazo.Sostenía al niño en su regazo","m. Hueco que forma una falda entre la cintura y las rodillas en una persona sentada. Guarda algunas castañas en el regazo","m. Lugar que ofrece amparo y consuelo. Volvió al regazo de su familia"],"https://cdn.pixabay.com/photo/2024/02/28/12/39/girl-8601996_1280.png",76],
+    ["Resquebrajarse",["tr. Hender ligera y a veces superficialmente algunos cuerpos duros, en especial la madera, la loza, el yeso, etc"],"https://cdn.pixabay.com/photo/2014/04/02/17/08/wood-308042_1280.png",69],
+    ["Sien",["f. Cada una de las dos partes laterales de la cabeza situadas entre la frente, la oreja y la mejilla"],"https://cdn.pixabay.com/photo/2022/06/01/05/09/happy-halloween-7234830_1280.png",59],
+    ["Simbiosis",["f. Biol. Asociación de individuos animales o vegetales de diferentes especies, sobre todo si los simbiontes sacan provecho de la vida en común"],"https://cdn.pixabay.com/photo/2020/09/13/07/13/anemone-5567486_1280.png",109],
+    ["Templaria",["adj. Dicho de una persona: De la orden de caballería del Temple, institución religiosa y militar fundada en el siglo XII, con la misión de asegurar los caminos de peregrinación a los Santos Lugares de Jerusalén","adj. Perteneciente o relativo a la Orden del Temple o a sus miembros. Castillo templario"],"https://cdn.pixabay.com/photo/2018/03/08/10/30/character-3208295_1280.png",124],
+    ["Versículos",["m. Cada una de las breves divisiones de los capítulos de ciertos libros, y singularmente de las Sagradas Escrituras","m. Parte del responsorio que se dice en las horas canónicas, regularmente antes de la oración","m. Cada uno de los versos de un poema escrito sin rima ni metro fijo, generalmente largo y con unidad de sentido"],"https://cdn.pixabay.com/photo/2016/10/06/19/02/book-1719737_1280.png",42],
+    ["Yuxtapuestos",["adj. Colocado junto a algo o en posición inmediata a algo."],"https://cdn.pixabay.com/photo/2012/04/18/20/29/sugar-37802_1280.png",75],
+    ["Zanjamos",["tr. Echar zanjas o abrirlas para fabricar un edificio o para otro fin","tr. Remover todas las dificultades e inconvenientes que puedan impedir el arreglo y terminación de un asunto o negocio"],"https://cdn.pixabay.com/photo/2014/03/24/13/45/co-workers-294266_1280.png",55],
+    ["",[""],"",],
+    ["",[""],"",],
+    ["",[""],"",],
+
 ]
 const listaelemento = document.getElementById("list")
 const listaespañola = document.getElementById("español")
@@ -10,6 +49,7 @@ for (let index = 0; index < listadepalabras.length; index++) {
     const palabra = listadepalabras[index][0];
     const definiciones = listadepalabras[index][1];
     const imagen = listadepalabras[index][2];
+    const pagina = listadepalabras[index][3];
     let palabraelemento = document.createElement("div");
     if (index<40){
     palabraelemento=listaespañola.appendChild(palabraelemento)
@@ -17,18 +57,39 @@ for (let index = 0; index < listadepalabras.length; index++) {
     palabraelemento=listacanaria.appendChild(palabraelemento)
     }
     palabraelemento.className="palabra"
-    palabraelemento.innerHTML=`
-    <div class="wordtitle"><strong>${palabra}:</strong></div>`
+    let titulo = document.createElement("div")
+    titulo = palabraelemento.appendChild(titulo)
+    titulo.className="wordtitle"
+    titulo.innerHTML=`<strong>${palabra}: +</strong>`
+    let contenido=null
+    titulo.onclick = function(){
+    if (contenido==null){
+    palabraelemento.style.borderColor = "blue"
+    contenido = document.createElement("div")
+    contenido = palabraelemento.appendChild(contenido)
+    contenido.className = "contenido"
+    titulo.innerHTML=`<strong>${palabra}: -</strong>`
     let lista = document.createElement("ul")
-    lista = palabraelemento.appendChild(lista)
-    for (let index = 0; index < definiciones.length; index++) {
-        const definicion = definiciones[index];
+    lista = contenido.appendChild(lista)
+    for (let idx = 0; idx < definiciones.length; idx++) {
+        const definicion = definiciones[idx];
         let newlistitem = document.createElement("li")
         newlistitem = lista.appendChild(newlistitem)
-        newlistitem.innerText = `${index+1}): ${definicion}`
+        newlistitem.innerText = `${idx+1}): ${definicion}`
+        
     }
     let newImage = document.createElement("img")
-    newImage = palabraelemento.appendChild(newImage)
+    newImage = contenido.appendChild(newImage)
     newImage.src = imagen
-
+    let newSub = document.createElement("div")
+    newSub = contenido.appendChild(newSub)
+    newSub.className = "pagina"
+    newSub.innerHTML = `<strong> Pág: ${pagina}</strong>`
+    }else{
+    contenido.remove()
+    contenido = null
+    palabraelemento.style.borderColor = "black"
+    titulo.innerHTML=`<strong>${palabra}: +</strong>`
+    }
+    }
 }
